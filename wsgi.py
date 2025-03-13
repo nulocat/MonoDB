72e48d80f17ec053aa3b9633f373d62305fa0031
nulocat <nulocat@proton.me>
2025-03-13 15:24:41 -0300

    refactor: adjust wsgi to match project structure, should work

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,9 @@
 import sys
-import os
 from pathlib import Path
 
 PROJECT_DIR = Path(__file__).resolve().parent
 sys.path.insert(0, str(PROJECT_DIR))
 
-os.environ["FLASK_APP"] = "app"
+from app import create_app
 
-from app import app as application
+application = create_app()
