ef84c2bfac67182cf62d8965112d05f359d31460
nulocat <nulocat@proton.me>
2025-03-13 15:09:23 -0300

    fix: use a fallback secret key generation for Flask app

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,13 @@ from flask import Flask, request, render_template
 from app.database import Database
 
 import os
+import secrets
 
 
 def create_app():
     app = Flask(__name__)
 
-    app.secret_key = os.environ['SECRET_KEY']
+    app.secret_key = os.environ.get('SECRET_KEY', secrets.token_urlsafe(32))
 
     @app.route("/api", methods=["POST"])
     def api_run():
