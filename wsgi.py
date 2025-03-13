90fb59e3395c467c7e18c1ce5f369575337d562a
nulocat <nulocat@proton.me>
2025-03-13 15:13:49 -0300

    refactor: restructure wsgi.py for a truely wsgi file

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,10 @@
-from app import create_app
+import sys
+import os
+from pathlib import Path
 
-app = create_app()
+PROJECT_DIR = Path(__file__).resolve().parent
+sys.path.insert(0, str(PROJECT_DIR))
+
+os.environ["FLASK_APP"] = "app"
+
+from app import app as application
